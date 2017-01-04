dc67c85f8c52c7a9e7ff1b184d1dccfa20e4665f
kuhlkrein <superseb.07@hotmail.fr>
2017-01-04 15:43:27 +0100

    Add files via upload

---
+++ elfsymbtab.c
@@ -0,0 +1,15 @@
+#include "elfsymbtab.h"
+
+
+Elf32_Sym * readSymbtab(Elf32_Ehdr header, Elf32_Shdr * Shtab,char * filePath, int isVerbose){
+	
+	for (j=0; j<header.e_shnum; j++){
+
+			
+
+	}
+
+int printSymbtab( Elf32_Sym * Symtab){
+
+	return 0;
+}
---
+++ elfsymbtab.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+
+#include "filereader.h"
+#include "elfsectiontab.h"
+
+Elf32_Sym * readSymbtab(Elf32_Ehdr header, Elf32_Shdr * tab,char * filePath, int isVerbose);
+
+int printSymbtab( Elf32_Sym * Symtab);
