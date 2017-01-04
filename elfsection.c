2f50b0379315f391f16a19b1c306b2ca78a019a6
Gueckmooh <brignon.enzo@gmail.com>
2017-01-04 16:56:40 +0100

    Add files via upload

--- elfsection.c
+++ elfsection.c
@@ -1,19 +1,17 @@
 #include "elfsection.h"
 
-int getindxsct(char * name, Elf32_Ehdr header, ){
-	
-}
 
-void affichage_section(char * filePath, Elf32_Ehdr header, Elf32_Shdr *tab, char *section_name, int section_index){
+void read_section(char * filePath, Elf32_Ehdr header, Elf32_Shdr *Shtab, char *section_name, int section_index){
 
-	int i,ind_name;
+	int i,ind_name, k, j;
 	unsigned char* fileBytes = readFileBytes(filePath);
 	Elf32_Shdr section;
 	int isValid = 1;
+	char name[256];
 
 	if(section_name[0] == '\0'){ // chaine vide faire avec l'index
 		if(!(section_index > header.e_shnum)){
-			section = tab[section_index-1];
+			section = Shtab[section_index-1];
 		}
 		else {
 			printf("Veuillez choisir un numéro de section valable\n");
@@ -21,23 +19,48 @@ void affichage_section(char * filePath, Elf32_Ehdr header, Elf32_Shdr *tab, char
 		}
 	} else { // sinon avec le nom de la section #galère
 			// Searching the name of the section
-			ind_name = header.e_shstrndx;
-			for (int k = 0; k < (int) tab[j].sh_name; k++) {
-				while (fileBytes[ind_name] != '\0')
-					ind_name++;
+		j = 0;
+		isValid = 1;
+		while (j < header.e_shnum && isValid != 0) {
+			ind_name = Shtab[header.e_shstrndx].sh_offset;
+			k = 0;
+			ind_name =  ind_name + Shtab[j].sh_name;
+			while(fileBytes[ind_name] != '\0'){
+				name[k] = fileBytes[ind_name];
+				k++;
 				ind_name++;
-			}		
-
+			}
+			name[k]='\0';
+			isValid = strcmp (name, section_name);
+			j++;
+		}
+		if (isValid!=0) {
+			printf("Veuillez donner un nom correct\n");
+			isValid = 0;
+		} else 
+			isValid = 1;
+		section = Shtab[j-1];
 
 	}
 	
 	if (isValid == 1) {
-		printf("Contenu de la section %d :", section.sh_name); // a completer avec String Table
-
+		ind_name = Shtab[header.e_shstrndx].sh_offset;
+		k = 0;
+		ind_name =  ind_name + section.sh_name;
+		while(fileBytes[ind_name] != '\0'){
+			name[k] = fileBytes[ind_name];
+			k++;
+			ind_name++;
+		}
+		name[k]='\0';
+		printf("Contenu de la section %s :", name); // a completer avec String Table
+		j = 0;
 		for(i=section.sh_offset;i<=section.sh_offset+section.sh_size;i++){
-
+		
 			printf("%x ", fileBytes[i]);
+			j++;
 		} 
 	}
+	printf("\n");
 
 }
--- elfsection.h
+++ elfsection.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <elf.h>
+#include <string.h>
 
-#ifndef __FILE_READER__
-#define  __FILE_READER__
+
 #include "filereader.h"
-#endif
 
 
-void affichage_section(char * filePath, Elf32_Shdr *tab);
+
+void read_section(char * filePath, Elf32_Ehdr header, Elf32_Shdr *htab, char *section_name, int section_index);
