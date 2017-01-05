c6e5151451b0ccfa30bb0ae6b2f1a17873f45a3f
TheSpyGeek <mathieurire@gmail.com>
2017-01-05 13:43:42 +0100

    maj main.c

--- main.c
+++ main.c
@@ -3,30 +3,55 @@
 #include <elf.h>
 #include <getopt.h>
 #include <sys/stat.h>
-
+#include <string.h>
 #include "elfheader.h"
 #include "elfsectiontab.h"
+#include "elfsection.h"
+#include "util.h"
+
+int isnumber(const char*s) {
+   char* e = NULL;
+   (void) strtol(s, &e, 0);
+   return e != NULL && *e == (char)0;
+}
 
 int main(int argc, char * argv[]){
 
 	Elf32_Ehdr *header;
-
+	Elf32_Shdr *shtab;
 	header = malloc(sizeof(Elf32_Ehdr));
 
+	char * file_used = "ARM.o";
+	char section[50];
 
-	if(readHeader("example2.o", header)){
+	if(readHeader(file_used, header)){
 		
-		aff_header(header);
+		//aff_header(header);
+		
+		shtab = malloc(sizeof(Elf32_Shdr)*header->e_shnum);
+
+		if(readSectTab(shtab, header, file_used)){
+
+			aff_Sheader(shtab, header, file_used);
+
+			printf("Choisir un nom ou un numero de section pour affichage complet :\n");
+			scanf("%s", section);
+			printf("\n");
+			if(isnumber(section)){
+				 
+				read_section(file_used, *header, shtab, "", atoi(section)+1);
+			}
+			else{
+				read_section(file_used, *header, shtab, section, 1);
+			}
 		
 
-
-
-
-
-
-		return 0;
+		} else {
+			return 1;
+		}
+	
 	} else {
 		return 1;
 	}
-
+	return 0;
 }
