bfd083cabefcd91a46c3e8d7ec6af61dbda526ec
kuhlkrein <supeerseb.07@hotmail.fr>
2017-01-06 09:12:05 +0100

    écriture des commentaires

--- filereader.c
+++ filereader.c
@@ -2,12 +2,12 @@
 
 unsigned char* readFileBytes(const char *name)
 {
-    FILE *fl = fopen(name, "r");
-    fseek(fl, 0, SEEK_END);
-    long len = ftell(fl);
-    unsigned char *ret = malloc(len);
+    FILE *fl = fopen(name, "r");	//ouverture du fichier
+    fseek(fl, 0, SEEK_END);		
+    long len = ftell(fl);		//calcul longueur du fichier
+    unsigned char *ret = malloc(len);	//allocation de la taille du fichier dans le tableau de char
     fseek(fl, 0, SEEK_SET);
-    fread(ret, 1, len, fl);
-    fclose(fl);
-    return ret;
+    fread(ret, 1, len, fl);		//remplissage du tableau de char
+    fclose(fl);				//fermeture du fichier
+    return ret;				//renvoi du tableau
 }
