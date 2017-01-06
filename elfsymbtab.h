6c7663abbf26950425cb63b973d5ae87042d9f56
kuhlkrein <superseb.07@hotmail.fr>
2017-01-06 11:48:02 +0100

    Update elfsymbtab.h

--- elfsymbtab.h
+++ elfsymbtab.h
@@ -8,7 +8,7 @@
 int getIndSectionSymtab(Elf32_Ehdr * header,Elf32_Shdr* shtab);
 
 /*Prend comme entree un Elf32_Ehdr et un Elf32_Shdr* et renvoit l'indice de la table de symbole Symtab de type 6 */
-int getIndSectionDynsym(Elf32_Ehdr header,Elf32_Shdr* shtab);
+int getIndSectionDynsym(Elf32_Ehdr * header,Elf32_Shdr* shtab);
 
 /*Prend comme entree un Elf32_Ehdr, un Elf32_Shdr*, un nom de fichier ainsi que l'indice de la table de symbole choisie 
 et rempli par effet de bord un Shtab,Elf32_Sym * qui sera le tableau de tous les symboles de la section d'indice indice_symtab */
