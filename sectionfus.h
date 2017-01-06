56ce7b01d622ecb95e1e5e1e81bff0b15b2b0a8c
kuhlkrein <supeerseb.07@hotmail.fr>
2017-01-06 15:07:21 +0100

    ajout des sectionfus

--- sectionfus.h
+++ sectionfus.h
@@ -6,9 +6,9 @@
 #include "elfsectiontab.h"
 
 typedef struct {
-	unsigned char name[256]	
+  char name[256]	;
 	unsigned char * sect;
 	int num;
 } sect_tab;
 
-void sectfusion (Elf32_Ehdr * header1, Elf32_Shdr shtab1, char *filepath1, Elf32_Ehdr * header2, Elf32_Shdr shtab2, char *filepath2, sect_tab * tab);
+void sectfusion (Elf32_Ehdr * header1, Elf32_Shdr * shtab1, const char *filepath1, Elf32_Ehdr * header2, Elf32_Shdr * shtab2, const char *filepath2, sect_tab * tab);
