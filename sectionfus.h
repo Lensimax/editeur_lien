afc003e1e1ee866b97e14bd558b15ad558709c44
Enzo Brignon <brignone@debby.lan>
2017-01-06 15:13:15 +0100

    maj

--- sectionfus.h
+++ sectionfus.h
@@ -6,9 +6,9 @@
 #include "elfsectiontab.h"
 
 typedef struct {
-  char name[256]	;
+	char name[256];
 	unsigned char * sect;
-	int num;
+	int num, fichier, numsection;
 } sect_tab;
 
 void sectfusion (Elf32_Ehdr * header1, Elf32_Shdr * shtab1, const char *filepath1, Elf32_Ehdr * header2, Elf32_Shdr * shtab2, const char *filepath2, sect_tab * tab);
