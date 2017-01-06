35ff92f414c695c82a7b06e806023eb0d9354ac2
kuhlkrein <supeerseb.07@hotmail.fr>
2017-01-06 09:32:17 +0100

    commentaire de elfheader

--- elfheader.c
+++ elfheader.c
@@ -6,18 +6,26 @@ int readHeader(char *filePath, Elf32_Ehdr *header){
 
 	FILE *f;
 
-	f = fopen(filePath, "r");
-
-	if(f != NULL){
-		fread(header, sizeof(Elf32_Ehdr), 1, f);
-		fclose(f);
-	} else {
+	//ouverture du fichier filePath
+	f = fopen(filePath, "r");		
+	
+	//si l'ouverture reussit
+	if(f != NULL){				
+		//on rempli le header
+		fread(header, sizeof(Elf32_Ehdr), 1, f);	
+		//fermeture du fichier
+		fclose(f);	
+	//si l'ouverture echoue		
+	} else {				
+		//message d'erreur
 		printf("Probleme ouverture fichier (header)\n");
 		return 0;
 	}
 
+	//si on a le chiffre magique suivit de ELF(=fichier ELF)
 	if(header->e_ident[EI_MAG0] == 127 && header->e_ident[EI_MAG1] == 69 && header->e_ident[EI_MAG2] == 76 && header->e_ident[EI_MAG3] == 70){
-	
+
+		//inversion des octets si on a un problème de boutisme	
 		if((header->e_ident[EI_DATA] == 1 && is_big_endian()) || ((header->e_ident[EI_DATA] == 2) && !is_big_endian())) {
 		header->e_type = reverse_2(header->e_type);
 		header->e_machine = reverse_2(header->e_machine);
@@ -34,7 +42,8 @@ int readHeader(char *filePath, Elf32_Ehdr *header){
 		header->e_shstrndx = reverse_2(header->e_shstrndx);
 		}
 		return 1;
-	} else {
+	//si on est pas dans un fichier ELF
+	} else {	
 		return 0;
 	}
 
@@ -49,26 +58,31 @@ int readHeader(char *filePath, Elf32_Ehdr *header){
 
 void aff_header(Elf32_Ehdr *header){
 
+	//affichage fichier ELF valide
 	if(header->e_ident[EI_MAG0] == 127 && header->e_ident[EI_MAG1] == 69 && header->e_ident[EI_MAG2] == 76 && header->e_ident[EI_MAG3] == 70){
 		printf("[*] Fichier ELF Reconnu\n");
 	}else{	printf("[E] Le fichier fourni n'est pas un fichier ELF\n");
 	}
 	
+	//affichage classe du fichier
 	if (header->e_ident[EI_CLASS] == 1){
 		printf("[*] Classe : 32 Bits ELFCLASS32\n");
 	}else{	printf("[E] Classe : Invalide\n");}
 	
+	//affichage boutisme fichier
 	switch(header->e_ident[EI_DATA]) {
 		case 1: printf("[*] EI_DATA : LSB Little endian\n"); break;
 		case 2: printf("[*] EI_DATA : MSB Big endian\n"); break;
 		default: printf("[E] Erreur de lecture de l'encodage EI_DATA\n"); break;
 	}
 	
+	//affichage version du header
 	switch(header->e_ident[EI_VERSION]) {
-		case 0: printf("[*] Version du header : %d\n",header->e_ident[EI_VERSION]); break;
-		default: printf("[E] Version du header invalide\n"); break;
+		case 0: printf("[E] Version du header invalide\n"); break;
+		default: printf("[*] Version du header : %d\n",header->e_ident[EI_VERSION]); break;
 	}
 	
+	//affichage PAD du fichier
 	switch(header->e_ident[EI_PAD]) {
 		case 0: printf("[*] ABI : UNIX SYSTEM V\n"); break;
 		case 3: printf("[*] ABI : LINUX\n"); break;
@@ -78,6 +92,7 @@ void aff_header(Elf32_Ehdr *header){
 		default: printf("[W] ABI : INCONNU\n"); break;
 	}
 	
+	//affichage type du fichier
 	switch(header->e_type) {
 		case 0: printf("[*] Pas de type de fichiers\n"); break;
 		case 1: printf("[*] Type du fichier : Relocatable file\n"); break;
@@ -86,7 +101,8 @@ void aff_header(Elf32_Ehdr *header){
 		case 4: printf("[*] Type du fichier : Core file\n"); break;
 		default: printf("[W] Type du fichier : Inconnu\n"); break;
 	}
-
+	
+	//affichage machine
 	switch(header->e_machine) {
 		case 0:printf("[*] Aucune machine cible\n"); break;
 		case 2:printf("[*] Machine cible : SPARC\n"); break;
@@ -102,31 +118,42 @@ void aff_header(Elf32_Ehdr *header){
 		default:printf("[W] Machine cible non reconnue\n"); break;
 	}
 
+	//affichage version du fichier
 	switch(header->e_version) {
 		case 0: printf("[E] Version invalide\n"); break;
 		case 1: printf("[*] Version : originale\n"); break;
 		default: printf("[*] Version : 0x%x\n",header->e_version); break;
 	}
 
+	//affichage point d'entree
 	printf("[*] Point d'entrée : 0x%x\n",header->e_entry);
 
-	printf("[*] Début des en-têtes de header : %d\n",header->e_phoff);
+	//affichage offset du debut des entetes de header
+	printf("[*] Début des entêtes de header : %d\n",header->e_phoff);
 
-	printf("[*] Début des en-têtes de sections : %d\n",header->e_shoff);
+	//affichage offset du debut des entetes de s
+	printf("[*] Début des entêtes de s : %d\n",header->e_shoff);
 
+	//affichage flags
 	printf("[*] Flags : 0x%x\n",header->e_flags);
 
+	//affichage taille du header
 	printf("[*] Taille du header : %d\n",header->e_ehsize);
 
+	//affichage taille d'une entree dans la table d'entete de programme
 	printf("[*] Taille d'une entrée dans la table contenant l'entête de programme : %d\n",header->e_phentsize);
 
+	//affichage nombre d'entrees dans la table
 	printf("[*] Nombre d'entrées dans la table contenant l'entête de programme : %d\n",header->e_phnum );
 
-	printf("[*] Taille d'une entrée dans la table des entêtes de sections  : %d\n",header->e_shentsize);
+	//affichage taille d'une entree dans la table des entetes
+	printf("[*] Taille d'une entrée dans la table des entêtes de s : %d\n",header->e_shentsize);
 
-	printf("[*] Nombre d'entrées dans la table des entêtes de sections : %d\n",header->e_shnum);
+	//affichage nombre d'entrees dans la table des entetes
+	printf("[*] Nombre d'entrées dans la table des entêtes de s : %d\n",header->e_shnum);
 
-	printf("[*] Indice des noms de sections : %d\n",header->e_shstrndx);
+	//affichage indice du nom des s
+	printf("[*] Indice des noms de s : %d\n",header->e_shstrndx);
 
 
 
