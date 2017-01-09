f348a429151b1deb5a15759a0c8ae93f4ffebf01
zackattack73 <loic.houdebine@wanadoo.fr>
2017-01-09 14:01:55 +0100

    Structure elf

---
+++ elfstruct.h
@@ -0,0 +1,23 @@
+#ifndef __ELF_STRUCT_H__
+#define __ELF_STRUCT_H__
+
+#include <elf.h>
+#include <stdio.h>
+
+typedef struct ELF_STRUCT {
+
+	FILE* file; // Nom du fichier, init en 1er
+	Elf32_Ehdr *header; //Elf header, init en 2e
+	Elf32_Shdr *shtab // 4e
+	Elf32_Sym *symtab; // 7e
+	Elf32_Rela *Relatab; // 8e
+	Elf32_Rel *Reltab; // 8e
+	unsigned char **sections_content; // malloc( sizeof(char *) * elf_struct->elf_header->e_shnum ), 5e
+	char *section_names;
+	int nb_sections; // elf_struct->elf_header->e_shnum;, init en 3e
+	int str_table_idx; // 6e
+	int symb_table_idx; // 6e
+
+} ELF_STRUCT;
+
+#endif 
