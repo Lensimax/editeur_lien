ff5dbc0706931650d5faa9a46cc573357d9de478
lucassA <lucas.albarede4b@laposte.net>
2017-01-10 12:49:36 +0100

    Add files via upload

--- elfstruct.h
+++ elfstruct.h
@@ -4,19 +4,30 @@
 #include <elf.h>
 #include <stdio.h>
 
-typedef struct STRUCT_REL
-{
-	Elf32_Rel *Reltab;
+typedef struct STRUCT_REL {
+
+	Elf32_Rel *reltab;
 	int indice_section;
+
 } STRUCT_REL;
+
+typedef struct STRUCT_SYM {
+
+	Elf32_Sym symbole;
+	int old_num;
+	int fusion_num_f1;
+	int fusion_num_f2;
+	int new_num;
+
+} STRUCT_SYM;
 
 typedef struct ELF_STRUCT {
 
 	FILE* file; // Nom du fichier, init en 1er
 	Elf32_Ehdr *header; //Elf header, init en 2e
 	Elf32_Shdr *shtab; // 4e
-	Elf32_Sym *symtab; // 7e
-	STRUCT_REL *tabrel;
+	STRUCT_SYM *symtab; // 7e
+	STRUCT_REL * tabrel; // 8e
 	unsigned char **sections_content; // malloc( sizeof(char *) * elf_struct->elf_header->e_shnum ), 5e
 	char *section_names;
 	int nb_sections; // elf_struct->elf_header->e_shnum;, init en 3e
@@ -30,5 +41,6 @@ typedef struct ELF_STRUCT {
 	unsigned char* fileBytes;
 
 } ELF_STRUCT;
+
 
 #endif 
