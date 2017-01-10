693ff2a2ce8f0c8b43b4585f6c53250f5fd125f3
TheSpyGeek <mathieurire@gmail.com>
2017-01-10 10:25:30 +0100

    maj structure

--- elfstruct.h
+++ elfstruct.h
@@ -10,8 +10,7 @@ typedef struct ELF_STRUCT {
 	Elf32_Ehdr *header; //Elf header, init en 2e
 	Elf32_Shdr *shtab; // 4e
 	Elf32_Sym *symtab; // 7e
-	Elf32_Rela *Relatab; // 8e
-	Elf32_Rel *Reltab; // 8e
+	Elf32_Rel **Reltab; // 8e
 	unsigned char **sections_content; // malloc( sizeof(char *) * elf_struct->elf_header->e_shnum ), 5e
 	char *section_names;
 	int nb_sections; // elf_struct->elf_header->e_shnum;, init en 3e
